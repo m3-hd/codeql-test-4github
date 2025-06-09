a6708ca17df612c558cb0c1efc66fb7e34ac0c6c
tyousinu <s-tyou@m3hd.co.jp>
2025-06-09 15:02:49 +0900

    追加

---
+++ apps/manage/frontend/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      <p>This is a simple index page for CodeQL testing purposes.</p>
+    </div>
+  );
+}
